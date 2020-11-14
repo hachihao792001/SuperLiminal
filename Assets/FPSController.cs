b4f0492b7201c0c252cd15bf56417481469a4433
hachihao792001 <hachihao792001@gmail.com>
2020-11-14 14:30:46 +0700

    Initial commit

---
+++ Assets/FPSController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSController : MonoBehaviour
+{
+    Rigidbody rb;
+
+    [Header("Mouse")]
+    public float sentivity; //2
+    public float yaw, pitch;
+
+    [Space]
+    [Header("Walking & Running")]
+    public Vector2 input;
+    public float walkSpeed; //3
+    public bool running;
+
+    [Space]
+    [Header("Jumping")]
+    public float groundDistance;    //1.1
+    public bool grounded;
+    public float jumpForce;     //200
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        rb = transform.parent.GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        //Mouse
+        /*
+        yaw -= Input.GetAxis("Mouse Y") * sentivity;
+        pitch += Input.GetAxis("Mouse X") * sentivity;
+
+        transform.parent.eulerAngles = new Vector3(0, pitch, 0);
+        transform.eulerAngles = new Vector3(yaw, pitch, 0);
+        */
+        transform.parent.Rotate(Vector3.up * Input.GetAxis("Mouse X") * sentivity);
+        transform.Rotate(Vector3.left * Input.GetAxis("Mouse Y") * sentivity);
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, 0);
+
+        //Walking & Running
+        running = Input.GetKey(KeyCode.LeftControl);
+        input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        input *= walkSpeed * (running ? 2 : 1);
+        rb.velocity = transform.parent.TransformDirection(new Vector3(input.x, rb.velocity.y, input.y));
+
+        //Jumping
+        grounded = Physics.Raycast(transform.parent.position, Vector3.down, groundDistance);
+        if (grounded && Input.GetKeyDown(KeyCode.Space))
+            rb.AddForce(Vector3.up * jumpForce);
+    }
+}
---
+++ Assets/SuperGrab.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SuperGrab : MonoBehaviour
+{
+    public Transform Grabbing;
+    public float originalDist;
+    public Vector3 originalScale;
+    public LayerMask IgnoreGrabbable;
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Grabbing == null)
+            {
+                RaycastHit rch;
+                if (Physics.Raycast(transform.position, transform.forward, out rch))
+                {
+                    if (rch.collider.tag == "Grabbable")
+                    {
+                        Grabbing = rch.collider.transform;
+                        Grabbing.GetComponent<Rigidbody>().isKinematic = true;
+                        //Grabbing.GetComponent<Collider>().enabled = false;
+                        Grabbing.parent = transform;
+
+                        originalScale = Grabbing.localScale;
+                        originalDist = Vector3.Distance(transform.position, Grabbing.position);
+                    }
+                }
+            }
+            else
+            {
+                Grabbing.GetComponent<Rigidbody>().isKinematic = false;
+                //Grabbing.GetComponent<Collider>().enabled = true;
+                Grabbing.parent = null;
+                Grabbing = null;
+            }
+        }
+
+        if (Grabbing != null)
+        {
+            RaycastHit wallRch;
+            if (Physics.Raycast(transform.position, transform.forward, out wallRch, 10000, IgnoreGrabbable))
+            {
+                Grabbing.position = wallRch.point;
+                Vector3 outDir;
+                float outDist;
+                Physics.ComputePenetration(Grabbing.GetComponent<Collider>(), Grabbing.position, Grabbing.rotation,
+                    wallRch.collider, wallRch.collider.transform.position, wallRch.transform.rotation,
+                    out outDir, out outDist);
+
+                Grabbing.position += (transform.position-wallRch.point).normalized* outDist;
+            }
+
+            Grabbing.localScale = originalScale * (Vector3.Distance(transform.position, Grabbing.position) / originalDist);
+        }
+    }
+}
